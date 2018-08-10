0ac5181e190653ec3cd1fcb4d5bfd0b248aab9e5
Haoran Feng <harrifeng@gmail.com>
2018-08-10 16:48:08 -0700

    g on

--- prepare.sh
+++ prepare.sh
@@ -10,5 +10,20 @@ deb-src http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe m
 deb-src http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
 deb-src http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
 " | sudo tee /etc/apt/sources.list
+
+echo "
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export HADOOP_HOME=/user/local/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+export HADOOP_MAPPED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_HOME/lib\"
+export JAVA_LIBRARY_PATH=$HADOOP_HOME/lib/native:$JAVA_LIBRARY_PATH
+" | sudo tee -a /home/vagrant/.bashrc
+
 sudo apt-get update
 sudo apt-get install -y git tmux lrzsz htop default-jdk
